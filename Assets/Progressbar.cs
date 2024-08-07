da8ef789bc411ed27e34cb8b897126f5152a31f4
BatmanVN <vumanhlinh16111995@gmail.com>
2024-08-07 19:41:12 +0700

    loading

---
+++ Assets/Progressbar.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public partial class Progressbar : MonoBehaviour
+{
+    public RectTransform recTransform;
+    public RectTransform mask;
+    public RectTransform progressImage;
+
+    private float maxWidth;
+    private float maxHeight;
+
+    private void Awake()
+    {
+        maxWidth = mask.rect.width;
+        maxHeight = mask.rect.height;
+    }
+
+    public void SetProgressValue(float progress)
+    {
+        float currentWidth = Mathf.Clamp01(progress) * maxWidth;
+        mask.sizeDelta = new Vector2(currentWidth, maxHeight);
+    }
+}
